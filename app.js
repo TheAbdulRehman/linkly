d2ca6084da26788d34e68199889760a95e0d9602
TheAbdulRehman <abdulrahman693.693@gmail.com>
2026-06-13 01:12:17 +0500

    removed un-necessary routes

--- app.js
+++ app.js
@@ -17,35 +17,6 @@ const getData=(req,res)=>{
     res.write(JSON.stringify({about:'this is about page'}));
     res.end();
   }
-   if(req.url==='/contact' && reqMethod==='GET'){
-    res.setHeader('Content-Type','application/json');
-    res.write(JSON.stringify({contact:'this is contact page'}));
-    res.end();
-  }
-   if(req.url==='/help' && reqMethod==='GET'){
-    res.setHeader('Content-Type','application/json');
-    res.write(JSON.stringify({help:'this is help page'}));
-    res.end();
-  }
-   if(req.url==='/services' && reqMethod==='GET'){
-    res.setHeader('Content-Type','application/json');
-    res.write(JSON.stringify({services:'this is services page'}));
-    res.end();
-  }
-   if(req.url==='/products' && reqMethod==='GET'){
-    const status=res.statusCode=200;
-    res.setHeader('Content-Type','application/json');
-    res.write(JSON.stringify({products:[{title:'this is a product',
-      description:'this is a product description'},
-      {title:'this is another product',
-        description:'this is another product description'  }],status,}));
-    res.end();
-  }
-   if(req.url==='/blog' && reqMethod==='GET'){
-    res.setHeader('Content-Type','application/json');
-    res.write(JSON.stringify({blog:'this is blog page'}));
-    res.end();
-  }
 }
 
 
